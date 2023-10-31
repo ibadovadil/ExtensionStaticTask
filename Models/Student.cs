7a401b28f4fda235d83f0569caec0544f7aeaef8
Adil <adilibadov654@gmail.com>
2023-11-01 01:49:12 +0400

    Task Added

---
+++ Models/Student.cs
@@ -0,0 +1,25 @@
+﻿namespace ExtensionStaticTask.Models
+{
+    internal class Student
+    {
+        private static int _id = 1;
+
+        public int Id { get; private set; }
+
+        public string Fullname { get; set; }
+
+
+        public string GroupNo { get; set; }
+
+        public int Age { get; set; }
+
+        public Student(string fullname, string groupNo, int age)
+        {
+            Id = _id;
+            _id++;
+            Fullname = fullname;
+            GroupNo = groupNo;
+            Age = age;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using ExtensionStaticTask.Models;
+
+namespace ExtensionStaticTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullname;
+            string groupNo;
+            int age;
+
+            Console.Write("Enter Name and Surname (Name Surname) ");
+            fullname = Console.ReadLine();
+
+            Console.Write("Enter group number  ");
+            groupNo = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+
+            Student student = new Student(fullname, groupNo, age);
+
+            Console.WriteLine($"Id: {student.Id}");
+            Console.WriteLine($"Fullname: {student.Fullname}");
+            Console.WriteLine($"GroupNo: {student.GroupNo}");
+            Console.WriteLine($"Age: {student.Age}");
+        }
+    }
+}
